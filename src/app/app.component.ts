07db4203471bf2e3f3d75a013f263348a77521ef
akarimi <ebicomp@gmail.com>
2023-09-16 16:56:04 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,24 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'pipes';
+  name: string = '';
+  date: string = '';
+  currency: string = '';
+  onInput(event: Event) {
+    const inputValue = (event.target as HTMLInputElement).value;
+    this.name = inputValue;
+  }
+
+  onInputDate(event: Event) {
+    const inputValue = (event.target as HTMLInputElement).value;
+    this.date = inputValue;
+  }
+  onCurrencyChange(event: Event) {
+    const inputValue = (event.target as HTMLInputElement).value;
+    this.currency = inputValue;
+  }
 }
